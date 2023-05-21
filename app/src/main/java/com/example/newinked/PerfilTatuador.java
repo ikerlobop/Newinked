2c13576a906aaed708b91b8cacf032b9cc5ecc87
unknown <ikerlobop@gmail.com>
2023-05-21 21:42:57 +0200

    Intentando hacer edit
    Edit

--- app/src/main/java/com/example/newinked/PerfilTatuador.java
+++ app/src/main/java/com/example/newinked/PerfilTatuador.java
@@ -1,24 +1,40 @@
 package com.example.newinked;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PerfilTatuador extends AppCompatActivity {
 
     private ImageView profileImageView;
     private Button editProfileButton;
-    private TextView userNameLabel;
-    private TextView userBioLabel;
+    private EditText userNameLabel;
+    private EditText userBioLabel;
     private TextView galleryLabel;
     private GridView galleryGridView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +51,7 @@ public class PerfilTatuador extends AppCompatActivity {
         Button addPhotoButton = findViewById(R.id.add_photo_button);
         Spinner tattooStyleSpinner = findViewById(R.id.tattoo_style_spinner);
 
+
         // Configurar adaptador para Spinner de estilos de tatuaje
         ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.tattoo_styles, android.R.layout.simple_spinner_item);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -44,10 +61,60 @@ public class PerfilTatuador extends AppCompatActivity {
         addPhotoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Aquí puedes agregar el código para permitir al usuario añadir fotos a su galería
+                // Aquí puedes agregar el código para añadir una foto a la galería
             }
         });
 
-        // Aquí puedes agregar el código para recuperar y mostrar la información de perfil del usuario
+        // Obtén la referencia del usuario actual
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser == null) {
+            // El usuario no está autenticado, muestra un mensaje de error o inicia sesión.
+            Toast.makeText(PerfilTatuador.this, "Usuario no autenticado", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String firebaseId = currentUser.getUid(); // Obtén la ID del usuario autenticado
+
+// Aquí puedes agregar el código para recuperar y mostrar la información de perfil de tatuadores
+
+// Si pulsamos el botón "Editar perfil"
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nombre = userNameLabel.getText().toString();
+                String biografia = userBioLabel.getText().toString();
+
+                if (TextUtils.isEmpty(nombre) || TextUtils.isEmpty(biografia)) {
+                    Toast.makeText(PerfilTatuador.this, "Por favor, ingresa nombre y biografía", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Actualizar solo el nombre y la biografía del tatuador
+                DatabaseReference tatuadorRef = FirebaseDatabase.getInstance().getReference("tatuadores").child(firebaseId);
+                tatuadorRef.child("nombre").setValue(nombre);
+                tatuadorRef.child("biografia").setValue(biografia)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                // Datos actualizados correctamente
+                                Toast.makeText(PerfilTatuador.this, "Perfil actualizado exitosamente", Toast.LENGTH_SHORT).show();
+
+                                // Realizar las actualizaciones necesarias en la interfaz de usuario si es necesario
+
+                                // Mostrar la biografía en el TextView "bio_nuevo"
+                                TextView bio_nuevo = findViewById(R.id.bio_nuevo);
+                                bio_nuevo.setText(biografia);
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Error al actualizar los datos
+                                Toast.makeText(PerfilTatuador.this, "Error al actualizar el perfil: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/newinked/Tatuador.java
+++ app/src/main/java/com/example/newinked/Tatuador.java
@@ -1,24 +1,24 @@
 package com.example.newinked;
 
 public class Tatuador {
+
     private String nombre;
     private String email;
     private String contrasena;
     private String ubicacion;
+    private String biografia;
 
-
-    public Tatuador(String nombre, String email, String contrasena, String ubicacion) {
-    }
-
-    public Tatuador(String nombre, String email) {
+    public Tatuador(String nombre, String email, String contrasena, String ubicacion, String biografia) {
         this.nombre = nombre;
         this.email = email;
         this.contrasena = contrasena;
         this.ubicacion = ubicacion;
+        this.biografia = biografia;
 
     }
 
     public String getNombre() {
+
         return nombre;
     }
 
@@ -35,4 +35,11 @@ public class Tatuador {
         return ubicacion;
     }
 
+    public String getBiografia() {
+        return biografia;
+    }
 }
+
+
+
+
